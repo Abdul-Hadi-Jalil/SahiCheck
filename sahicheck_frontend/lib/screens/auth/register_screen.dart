e2f7373f04fb9b0aedeceddba4393c29e1197b1f
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2026-04-13 03:30:58 +0500

    register screen is now functional

--- sahicheck_frontend/lib/screens/auth/register_screen.dart
+++ sahicheck_frontend/lib/screens/auth/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -9,8 +10,87 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text(
+            "Create and account",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const Text("Join the community of digital guardians today"),
+          const SizedBox(height: 20),
+          Column(
+            children: [
+              Text('Email Address'),
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(hintText: 'Enter your email'),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+
+          Column(
+            children: [
+              Text('Password'),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Enter your password',
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+
+          Column(
+            children: [
+              Text('Confirm Password'),
+              TextFormField(
+                controller: confirmPasswordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Confirm your password',
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () async {
+              FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: emailController.toString().trim(),
+                password: passwordController.toString().trim(),
+              );
+            },
+            child: Text('Sign Up'),
+          ),
+
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Text("Already have an account?"),
+              TextButton(
+                onPressed: () {
+                  // navigate to registration screen
+                  widget.toggleScreen!();
+                },
+                child: Text('Login up'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
